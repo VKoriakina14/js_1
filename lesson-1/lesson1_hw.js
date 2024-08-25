fe7db288cf11c52d228d20dfa12e547c15f948b5
PopovYehor2802 <popovvvyehoor@gmail.com>
2024-08-25 12:04:10 +0300

    modified file

--- lesson-1/lesson1_hw.js
+++ lesson-1/lesson1_hw.js
@@ -1,58 +1,58 @@
  //Task - 1
 //Виберіть підходящі імена для змінних та створіть їх в коді.//
       //  1) Змінна для зберігання дати створеного файла.//
-         let fileCreationDate;
+  /*        let fileCreationDate;
          fileCreationDate = 15;
-         console.log(fileCreationDate);
+         console.log(fileCreationDate); */
     
-    // 2) Змінна для зберігання ім'я файлу. //
-        let fileName;
+//     2) Змінна для зберігання ім'я файлу. //
+      /*   let fileName;
         fileName = "2324";
-        console.log(fileName);
+        console.log(fileName); */
       
-        // 3) Змінна для зберігання кількості помилок. //
-        let numberOfErrors;
-        numberOfErrors = 1234;
-        console.log(numberOfErrors);
+        //3) Змінна для зберігання кількості помилок. //
+      /*   let savingErrors;
+        savingErrors = 1234;
+        console.log(savingErrors); */
 /* Task - 2
         1) Об'явіть дві змінні: login и name
         2) Змінній name в якості значення привласніть своє ім'я
         3) Скопіюйте значення із змінної name в login
         4) Виведіть в консолі  значення змінної login */
-        let login;
+       /*  let login;
         let nameUser;
         nameUser = 'Yehor';
         login = nameUser;
-        console.log(login);
+        console.log(login); */
 /* Task - 3
         Створіть 3 змінні в одному рядку з різними значеннями */
-        let log12 =12, log123 = 34, log432 = "Створюй адекватні назви для змінних!";
-        console.log(log12, log123, log432);
+       /*  let log12 =12, log123 = 34, log432 = "Створюй адекватні назви для змінних!";
+        console.log(log12, log123, log432); */
 /* Task - 4
         Створіть змінну з іменем myUndefined і встановіть її значення рівним undefined без використання слова "undefined" 
         %%% https://www.tiktok.com/@devslopes/video/7150509431485107498 %%%
         */
-        var myUndefined;
-        console.log(myUndefined);
+        /* var myUndefined;
+        console.log(myUndefined); */
 /* Task - 5
         Вам потрібно записати рядок "I'm lovin' it!" у змінну resultLine.
         - Значення змінних line1, line2, line3 не змінювати.
 */
-       let line1 = "Lovin";
+      /*  let line1 = "Lovin";
         let line2 = "It!";
         let line3 = "I'm";
 
         let resultLine = line3 + ' ' + line1 + ' ' + line2; //change this line
 
         console.log(resultLine); 
-
+ */
 /* Task - 6
         Вам потрібно записати рядок "70" у змінну digits.
         Використовуйте змінні x, y, z, порожній рядок та конкатенацію рядків.
 
         - Значення змінних x, y, z не змінюйте.
 */
-
+/* 
     let x = 3;
     let y = 4;
     let z = 0;
@@ -60,7 +60,7 @@
     let digits = "" + (x + y) + z;; //change this line
 
     console.log(digits); 
-
+ */
 /* Task - 7
         Вам потрібно записати число 100500 у змінну hugeAmount.
         Використовуйте змінні bigAmount, greatAmount та перетворення рядка у число.
@@ -69,14 +69,14 @@
         - Не змінюйте значення змінних bigAmount і greatAmount.
         - Використовуйте bigAmount, greatAmount та метод Integer() для ініціалізації змінної hugeAmount.
 */
-
+/* 
      let bigAmount = "500";
         let greatAmount = "100000";
 
         let hugeAmount = Number(bigAmount) + Number(greatAmount); //change this line
 
         console.log(hugeAmount); 
-
+ */
 /* Task - 8
         Змінити код так, щоб замість значень рядків виводилась довжина кожного рядка.
 
@@ -85,12 +85,12 @@
         - Для трьох рядків, переданих у метод console.log, слід викликати метод length.
         - Програма повинна вивести три числа, кожне з нового рядка.
 */
-
+/* 
 let emptyString = "";
 
         console.log(emptyString.length);
         console.log("Think Different".length);
-        console.log((emptyString + 2 + 2 + "22").length);
+        console.log((emptyString + 2 + 2 + "22").length); */
 
 /*Task - 9
         На екран виводиться 4 рядки. Усі вони є яскравим прикладом зловживання великими літерами.
@@ -119,12 +119,12 @@ let emptyString = "";
         - Викличте метод toUpperCase для трьох рядків, переданих методу console.log.
 */
 
-         let caps = "if I type in caps ";
+  /*        let caps = "if I type in caps ";
          let fbi = "фбр";
 
     console.log(fbi.toUpperCase());
     console.log("Гекльберрі Фінн".toUpperCase());
-    console.log(caps.toUpperCase() + "they know I mean business".toUpperCase()); 
+    console.log(caps.toUpperCase() + "they know I mean business".toUpperCase());  */
 
 /* Task - 11
         Додайте операції множення, ділення та віднімання.
@@ -147,7 +147,7 @@ let emptyString = "";
         Рядкове значення має бути створене на основі значення змінних name, age, company, position, наприклад:
         Привіт, мене звати Максим. Мені 29 років. Я працюю на посаді QA в компанії Apple.
 */
-
+       
 /*  let name = "Максим";
         let age = 29;
         let company = "Apple";
